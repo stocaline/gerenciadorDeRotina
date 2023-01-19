f9b1381b711e19f77f4667794a629f3a11d26647
stocaline <stocaline2@gmail.com>
2023-01-18 21:54:26 -0300

    Criando rotas, post de habitos e get habitos do dia

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,12 @@
 import Fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
 import cors from '@fastify/cors'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
+app.register(appRoutes)
 
-app.get('/', async () => {
-    const habits = await prisma.habbit.findMany({
-        where: {
-            title:{
-                startsWith: 'Beber'
-            }
-        }
-    })
-    return habits
-})
 
 app.listen({
     port: 3333,
