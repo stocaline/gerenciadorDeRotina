5396deb04591410dfe19ec61bbcc8ef7c4623363
stocaline <stocaline2@gmail.com>
2023-01-22 17:12:03 -0300

    Integrando backend(server) com web e mobile

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs"
+import { useEffect, useState } from "react"
+import { api } from "../lib/axios"
 import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
@@ -6,7 +9,23 @@ const summuryDates = generateDatesFromYearBeginning()
 const minimumSummaryDatesSiza = 18 * 7 // 18 weeks
 const amountOfDaysToFill = minimumSummaryDatesSiza - summuryDates.length
 
+type Summary = {
+    id: string,
+    date: string,
+    amount: number,
+    completed: number;
+}[]
+
+
 export function SummaryTable() {
+    const [summary, setSummary] = useState<Summary>([])
+
+    useEffect(() => {
+        api.get('summary').then(response => {
+            setSummary(response.data)
+        })
+    }, [])
+
     return (
         <div className="w-full flex">
             <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -21,11 +40,16 @@ export function SummaryTable() {
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summuryDates.map(date => {
+                    const dayInSummary = summary.find(day => {
+                        return dayjs(date).isSame(day.date, 'day')
+                    })
+
                     return (
                         <HabitDay
                             key={date.toString()}
-                            amount={5}
-                            completed={Math.random() * 5}
+                            date={date}
+                            amount={dayInSummary?.amount}
+                            completed={dayInSummary?.completed}
                         />
                     )
                 })}
